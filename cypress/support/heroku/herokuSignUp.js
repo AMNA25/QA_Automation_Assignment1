d7690a734d4600b0b8fd793dd3d911a13cacfa11
AMNA25 <amna.bibi@emumba.com>
2020-09-23 15:42:19 +0500

    Added Functions

--- cypress/support/heroku/herokuSignUp.js
+++ cypress/support/heroku/herokuSignUp.js
@@ -1,9 +1,9 @@
 import sign_up from '../../../pageObjects/herokuapp/sign_up'
-export default(a,b,c,d)=>{
+export default(a,b,c,d,e)=>{
     sign_up.typeFirstName(a) 
     sign_up.typeLastName(b) 
     sign_up.typeEmail(c)
     sign_up.typePassword(d) 
-    sign_up.typeRetypePassword(d)
+    sign_up.typeRetypePassword(e)
     sign_up.clickAlreadyaUser()
 }
